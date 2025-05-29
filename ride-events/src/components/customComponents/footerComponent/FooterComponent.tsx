00e8342a0c2abea803fa4d7341e2e33a7177a073
Akarsh Saraff <saraffakarsh@gmail.com>
2025-05-30 00:51:10 +0530

    Update FooterComponent.tsx

--- ride-events/src/components/customComponents/footerComponent/FooterComponent.tsx
+++ ride-events/src/components/customComponents/footerComponent/FooterComponent.tsx
@@ -3,7 +3,7 @@ import { Layout } from 'antd';
 const {  Footer  } = Layout;
 
 export const Footercomponent = ()=> {
-    return <Footer style={{ textAlign: 'center' }}>
+    return <Footer style={{ textAlign: 'center', borderTop: '1px solid lightgray' }}>
     Ride events ©{new Date().getFullYear()} Created by Ride events pvt ltd.
   </Footer>;
 };
